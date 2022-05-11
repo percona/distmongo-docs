83ed0eaed3dfd0542106bce6c3abea5e37098595
Anastasia Alexandrova <anastasia.alexandrova@percona.com>
2022-05-11 10:12:56 +0200

    DISTMONGO-146 Merged material theme into main config (#69)
    Added 404 page
    Added the Edit this page link
    
    modified:   Makefile
    new file:   source/404.rst
    modified:   source/conf.py
    deleted:    source/conf-material/conf.py

--- source/conf.py
+++ source/conf.py
@@ -35,10 +35,23 @@ sys.path.append(os.path.abspath('ext'))
 author = "Percona LLC and/or its affiliates 2015-2022"
 extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo', 
               'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 
-              'sphinx.ext.extlinks']
+              'sphinx.ext.extlinks', 'sphinx_gitstamp',
+              'sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+#templates_path = ['_templates']
+templates_path = ['_templates/theme']
+
+# Path to custom css files. These will override the default css attribute if they exist
+html_css_files = [
+    '../_static/css/material.css',
+]
+
+# Extension configuration
+gitstamp_fmt = "%b %d, %Y"
+# Specify the text pattern that won't be copied with the code block contents
+copybutton_prompt_text = '$'
+
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -148,16 +161,33 @@ extlinks = {'jirabug': ('https://jira.percona.com/browse/%s', '')
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'percona-theme'
+#html_theme = 'percona-theme'
+html_theme = 'sphinx_material'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'base_url': 'http://bashtage.github.io/sphinx-material/',
+    'repo_url': 'https://github.com/percona/distmongo-docs',
+    'repo_name': 'percona/distmongo-docs',
+    'color_accent': 'grey',
+    'color_primary': 'orange',
+    'google_analytics_account': 'UA-343802-3',
+    'globaltoc_collapse': True,
+    'version_dropdown': True,
+    'version_dropdown_text': 'Versions',
+    'version_info': {
+        "4.2": "https://docs.percona.com/percona-distribution-for-mongodb/4.2/",
+        "4.4": "https://docs.percona.com/percona-distribution-for-mongodb/4.4/",
+        "5.0": "https://docs.percona.com/percona-distribution-for-mongodb/5.0/",
+        "Latest": "https://docs.percona.com/percona-distribution-for-mongodb/latest/index.html"
+    },
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['.', './percona-theme']
+#html_theme_path = ['.', './percona-theme']
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
@@ -179,13 +209,13 @@ html_short_title = ' '.join([project, version])
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #
-html_logo = 'percona-server-logo.jpg'
+html_logo = '_static/images/percona-logo.svg'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
 #
-# html_favicon = None
+html_favicon = '_static/images/percona_favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -212,7 +242,7 @@ html_static_path = ['_static']
 # Custom sidebar templates, maps document names to template names.
 #
 html_sidebars = {
-        '**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'edit.html', 'searchbox.html'],
+        '**': ['globaltoc.html', 'searchbox.html', 'localtoc.html', 'logo-text.html', 'edit.html'],
         'using/windows': ['windowssidebar.html'],
 }
 
