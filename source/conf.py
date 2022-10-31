946412b90551bcc6cdb3db56e9b6273f73006b9f
Anastasia Alexandrova <anastasia.alexandrova@percona.com>
2022-10-31 21:38:05 +0100

    DISTMONGO-193 Added 6.0 to versions switcher (#96)

--- source/conf.py
+++ source/conf.py
@@ -183,7 +183,8 @@ html_theme_options = {
         "4.2": "https://docs.percona.com/percona-distribution-for-mongodb/4.2/",
         "4.4": "https://docs.percona.com/percona-distribution-for-mongodb/4.4/",
         "5.0": "https://docs.percona.com/percona-distribution-for-mongodb/5.0/",
-        "Latest": "https://docs.percona.com/percona-distribution-for-mongodb/latest/index.html"
+        "6.0": "https://docs.percona.com/percona-distribution-for-mongodb/6.0/",
+        "Latest": "https://docs.percona.com/percona-distribution-for-mongodb/latest/"
     },
 }
 
